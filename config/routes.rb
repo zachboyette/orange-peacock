47c2b5c44e4dca701d360b87e0119d3a4b319e0b
Zach <jzboyette@gmail.com>
2014-05-17 20:25:40 -0500

    added builder engine to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -56,3 +56,5 @@ APPNAME::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
+mount WebsiteBuilderEngine::Engine, :at => '/builder'
